fa0750429427ff9cfb93ef4b6f3165a8f7d054ca
MPhong03 <dangminhphong912@gmail.com>
2023-07-03 20:16:29 +0700

    updated by Phong

---
+++ SpringMVCProject/src/main/java/com/cnjava/spring_mvc_project/SpringMVCProject/App.java
@@ -0,0 +1,13 @@
+package com.cnjava.spring_mvc_project.SpringMVCProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
