0b5b3841971aadfb0270c20fecce2649e5c611cb
MPhong03 <dangminhphong912@gmail.com>
2023-07-22 08:24:42 +0700

    update database

--- javaweb.sql
+++ javaweb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 21, 2023 at 05:29 AM
+-- Generation Time: Jul 22, 2023 at 03:23 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -89,7 +89,11 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`productid`, `description`, `imagelink`, `price`, `productname`, `brandid`, `categoryid`) VALUES
-(13, 'Bàn phím laptop được thiết kế khoa học với cấu trúc liền khối cùng hành trình phím ngắn. Nhờ vậy bạn có thể bấm phím thoải mái, dễ dàng không bị dính phím. đồng thời tiếng động gây ra từ bàn phím khá nhỏ không làm ồn đến người dùng cũng như môi trường xung quanh.', 'unknown', 11690000, 'Laptop ASUS VivoBook R564JA-UH31T', 5, 2);
+(13, 'Bàn phím laptop được thiết kế khoa học với cấu trúc liền khối cùng hành trình phím ngắn. Nhờ vậy bạn có thể bấm phím thoải mái, dễ dàng không bị dính phím. đồng thời tiếng động gây ra từ bàn phím khá nhỏ không làm ồn đến người dùng cũng như môi trường xung quanh.', '/img/products-image-list/laptop-asus-vivobook-r564ja-uh51t-1.jpg;/img/products-image-list/laptop-asus-vivobook-r564ja-uh51t-2.jpg;/img/products-image-list/laptop-asus-vivobook-r564ja-uh51t-3.jpg', 11690000, 'Laptop ASUS VivoBook R564JA-UH31T', 5, 2),
+(15, 'Samsung từng bước khẳng định thương hiệu của mình bằng việc cho ra đời những mẫu flagship ngày càng hiện đại với công nghệ tiến bộ. Điển hình là chiếc điện thoại Samsung Galaxy S21 Ultra 5G  tích hợp mọi tính năng gần như trở thành mẫu smartphone hoàn hảo nhất.', 'unknown', 9990000, 'Samsung Galaxy S21 Ultra 5G Korea (Likenew)', 1, 1),
+(16, 'Hoàn toàn xứng đáng với những gì được mong chờ, phiên bản cao cấp nhất iPhone Xs Max 64 GB của Apple năm nay nổi bật với chip A12 Bionic mạnh mẽ, màn hình rộng đến 6.5 inch, cùng camera kép trí tuệ nhân tạo và Face ID được nâng cấp.', 'unknown', 8990000, 'Apple iPhone Xs Max 256GB Quốc Tế (Likenew)', 2, 1),
+(17, 'Tai nghe Bluetooth AirPods Pro MagSafe Charge Apple MLWK3 trắng được chế tác với vẻ ngoài tinh giản, gam màu trắng trẻ trung, sáng đẹp, phối hợp tuyệt vời với mọi trang phục từ đời thường đến công sở, dự tiệc của bạn. ', 'unknown', 4690000, 'Apple AirPods Pro 2021 Magsafe', 2, 6),
+(18, 'Black Shark 4 là điện thoại chơi game hàng đầu của Xiaomi trong năm 2021. Những điểm mạnh của Black Shark 4 là tương đối toàn diện. Thiết kế toàn màn hình cao cấp với tốc độ làm mới 144Hz/720Hz. Phần cứng là bộ vi xử lý Snapdragon 870 mạnh mẽ. Tản nhiệt cũng được nâng cấp mạnh mẽ giảm tới 18 độ cho CPU giúp chơi game cấu hình cao không nóng máy. Hơn nữa, Black Shark 4 đã xác lập kỷ lục với pin 4500mAh, hỗ trợ sạc 120W “nhanh nhất thế giới”,… Mang lại hiệu năng vượt trội về nhiều mặt.', 'unknown', 5790000, 'Xiaomi Black Shark 4 (8GB/128GB) Likenew', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -112,7 +116,16 @@ INSERT INTO `productvalue` (`valueid`, `valueindex`, `valuename`, `productid`) V
 (4, 'Intel Core I3-1005G1 1.20 GHz up to 3.40 GHz', 'Loại CPU', 13),
 (5, 'Intel UHD Graphics', 'Loại card đồ họa', 13),
 (6, '4GB', 'Dung lượng RAM', 13),
-(7, 'Tai nghe Bluetooth Apple AirPods 2 | Chính hãng Apple Việt Nam', '12', 13);
+(8, '6.8 inches', 'Kích thước màn hình', 15),
+(9, 'Exynos 2100', 'Chipset', 15),
+(12, '6.5 inches', 'Kích thước màn hình', 16),
+(13, 'Super Retina OLED', 'Công nghệ màn hình', 16),
+(14, '4GB', 'Dung lượng RAM', 16),
+(15, '64GB', 'Bộ nhớ trong', 16),
+(18, '1', 'test1', 17),
+(19, '2', 'test2', 17),
+(23, '6.67 inches', 'Kích thước màn hình', 18),
+(24, 'Super AMOLED 144Hz', 'Công nghệ màn hình', 18);
 
 -- --------------------------------------------------------
 
@@ -189,13 +202,13 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `productid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `productid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `productvalue`
 --
 ALTER TABLE `productvalue`
-  MODIFY `valueid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `valueid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `user`
